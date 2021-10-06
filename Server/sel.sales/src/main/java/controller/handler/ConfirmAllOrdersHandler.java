93502c755ec31dc0a9bb6f76f79b671b7887e77d
AlpTorac <a.torac@hotmail.com>
2021-10-06 02:26:32 +0200

    resolved conflicts

---
+++ Server/sel.sales/src/main/java/controller/handler/ConfirmAllOrdersHandler.java
@@ -0,0 +1,14 @@
+package controller.handler;
+
+import controller.IController;
+
+public class ConfirmAllOrdersHandler extends BusinessEventHandler {
+	public ConfirmAllOrdersHandler(IController controller) {
+		super(controller);
+	}
+
+	@Override
+	public void handleApplicationEvent(Object[] args) {
+		this.getController().confirmAllOrders();
+	}
+}
