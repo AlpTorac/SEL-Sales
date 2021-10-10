f3a21c1a3b2eed9466ab337add00f0d02f51743a
AlpTorac <a.torac@hotmail.com>
2021-10-10 03:06:15 +0200

    Removed the synchronized block

--- Server/sel.sales/src/main/java/dummyclient/ClientMain.java
+++ Server/sel.sales/src/main/java/dummyclient/ClientMain.java
@@ -17,20 +17,18 @@ public class ClientMain {
 		view.startUp();
 		external = new ClientExternal(controller, model);
 		external.connectToService();
-		final Object lock = new Object();
 		int i = 4;
 		while (true) {
-			synchronized (lock) {
-				IMessage order = new Message(MessageContext.ORDER, null, "order"+i+"-20210809000000111-1-1:item3,5;");
-				external.sendMessage(order);
-				System.out.println("Message Sent");
-				i++;
-				try {
-					lock.wait(3000);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
+			IMessage order = new Message(MessageContext.ORDER, null, "order"+i+"-20210809000000111-1-1:item3,5;");
+			external.sendMessage(order);
+			System.out.println("Message Sent");
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
+			i++;
 		}
 //		try {
 //			Thread.sleep(10000);
